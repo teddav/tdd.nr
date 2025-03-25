452b5f6d1156ecce885c345a50991b2eaebe4a38
teddav <teddav@users.noreply.github.com>
2025-03-25 15:40:44 +0100

    recursive ultrahonk

--- examples/README.md
+++ examples/README.md
@@ -6,4 +6,10 @@ CIRCUIT_NAME="tdd_id"
 bb prove -b "./target/$CIRCUIT_NAME.json" -w "./target/$CIRCUIT_NAME.gz" -o ./proof
 bb write_vk -b "./target/$CIRCUIT_NAME.json" -o ./proof
 bb verify -k ./proof/vk -p ./proof/proof
+
+# bb OLD_API write_recursion_inputs_ultra_honk -b "./target/first.json" -o ./proof --recursive
+
+# as fields
+bb prove -b "./target/$CIRCUIT_NAME.json" -w "./target/$CIRCUIT_NAME.gz" -o ./proof --output_format fields
+bb write_vk -b "./target/$CIRCUIT_NAME.json" -o ./proof --output_format fields
 ```
--- examples/recursive/nr.js
+++ examples/recursive/nr.js
@@ -3,12 +3,16 @@ const { Noir } = require("@noir-lang/noir_js");
 const { assert } = require("console");
 const fs = require("fs");
 const os = require("os");
+const { execSync } = require("child_process");
 
-async function prove() {
-  const circuit1 = JSON.parse(fs.readFileSync("./first/target/first.json"));
+const CIRCUITS = {
+  first: JSON.parse(fs.readFileSync("./first/target/first.json")),
+  recurse: JSON.parse(fs.readFileSync("./recurse/target/recurse.json")),
+};
 
-  const noir = new Noir(circuit1);
-  const backend = new UltraPlonkBackend(circuit1.bytecode, { threads: os.cpus() });
+async function prove_UltraPlonk() {
+  const noir = new Noir(CIRCUITS.first);
+  const backend = new UltraPlonkBackend(CIRCUITS.first.bytecode, { threads: os.cpus() });
 
   const data = {
     a: 3,
@@ -36,10 +40,9 @@ async function prove() {
   const isValid = await backend.verifyProof(proof);
   assert(isValid);
 
-  //   RECURSIVE
-  const circuit_recursive = JSON.parse(fs.readFileSync("./recurse/target/recurse.json"));
-  const noir_recursive = new Noir(circuit_recursive);
-  const backend_recursive = new UltraPlonkBackend(circuit_recursive.bytecode, { threads: os.cpus() });
+  // RECURSIVE
+  const noir_recursive = new Noir(CIRCUITS.recurse);
+  const backend_recursive = new UltraPlonkBackend(CIRCUITS.recurse.bytecode, { threads: os.cpus() });
 
   const recursiveInputs = {
     verification_key: vkAsFields,
@@ -55,4 +58,52 @@ async function prove() {
   assert(verified);
 }
 
-prove();
+async function prove_UltraHonk() {
+  execSync("bb prove -b ./first/target/first.json -w ./first/target/first.gz -o ./first/proof --output_format fields");
+  execSync("bb write_vk -b ./first/target/first.json -o ./first/proof --output_format fields");
+
+  const noir = new Noir(CIRCUITS.first);
+  const backend = new UltraHonkBackend(CIRCUITS.first.bytecode, { threads: os.cpus() });
+
+  const data = {
+    a: 3,
+    b: 4,
+  };
+
+  const { witness } = await noir.execute(data);
+
+  //   PROOF
+  console.time("prove");
+  const proof = await backend.generateProof(witness);
+  console.timeEnd("prove");
+  console.log("proof", proof);
+
+  // // VK
+  // const publicInputsCount = 1;
+  const proofAsFields = JSON.parse(fs.readFileSync("./first/proof/proof_fields.json"));
+  const vkAsFields = JSON.parse(fs.readFileSync("./first/proof/vk_fields.json"));
+  const vkHash = "0x" + "0".repeat(64);
+
+  // VERIFY proof1
+  const isValid = await backend.verifyProof(proof);
+  assert(isValid);
+
+  // RECURSIVE
+  const noir_recursive = new Noir(CIRCUITS.recurse);
+  const backend_recursive = new UltraHonkBackend(CIRCUITS.recurse.bytecode, { threads: os.cpus() });
+
+  const recursiveInputs = {
+    verification_key: vkAsFields,
+    proof: proofAsFields,
+    public_inputs: proof.publicInputs,
+    key_hash: vkHash,
+  };
+
+  console.log("recursing...", recursiveInputs);
+  const { witness: witness_recursive } = await noir_recursive.execute(recursiveInputs);
+  const proof_recursive = await backend_recursive.generateProof(witness_recursive);
+  const verified = await backend_recursive.verifyProof(proof_recursive);
+  assert(verified);
+}
+
+prove_UltraHonk();
